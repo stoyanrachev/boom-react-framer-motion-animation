4209c405c57e68ca457d5bdfb76ac14faf7281a9
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-01-24 18:08:51 +0200

    add animation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { motion } from "framer-motion";
 
 function App() {
+  const variants = {
+    hidden: { opacity: 0, x: -800, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: -100 },
+  }
   return (
     <div className="App">
-      <form>
+      <motion.form
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        variants={variants}
+        transition={{ type: 'linear' }}
+      >
         <h1>Create An Account</h1>
         <label>
           Email:
@@ -14,7 +26,7 @@ function App() {
           <input name="password" type="password" required />
         </label>
         <button>Sign Up!</button>
-      </form>
+      </motion.form>
     </div>
   );
 }
